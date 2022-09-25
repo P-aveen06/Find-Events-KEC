dc47d66c87493f49f45017cce516a2c86acb49a0
P-aveen06 <paveenpradeep2@gamil.com>
2022-09-25 21:42:46 +0530

    Signin Component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import SignIn from './Pages/SignIn'
+const App=() =>{
+ return (<div className='App m-auto width-1024px height-100'>
+   <SignIn/>
+ </div>);
+}
+
+export default App;
---
+++ src/Pages/SignIn.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Checkbox } from "primereact/checkbox";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "./signin.css";
+const SignIn = () => {
+  const [checked1, setChecked1] = useState("");
+  return (
+    <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6 m-auto align-center">
+      <div className="text-center mb-5">
+        <i className="pi pi-sign-in font-3rem"></i>
+        <div className="text-900 text-3xl font-medium mb-3">OOPs! Sign In</div>
+        <span className="text-600 font-medium line-height-3">
+          Don't have an account?
+        </span>
+        <a
+          className="font-medium no-underline ml-2 text-blue-500 cursor-pointer"
+          href="*"
+        >
+          Create today!
+        </a>
+      </div>
+
+      <div>
+        <label htmlFor="email" className="block text-900 font-medium mb-2">
+          Email
+        </label>
+        <InputText type="text" className="w-full mb-3" />
+
+        <label htmlFor="password" className="block text-900 font-medium mb-2">
+          Password
+        </label>
+        <InputText type="password" className="w-full mb-3" />
+
+        <div className="flex align-items-center justify-content-between mb-6">
+          <div className="flex align-items-center">
+            <Checkbox
+              id="rememberme"
+              className="mr-2"
+              checked={checked1}
+              onChange={(e) => setChecked1(e.checked)}
+            />
+            <label htmlFor="rememberme">Remember me</label>
+          </div>
+          <a
+            className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"
+            href="*"
+          >
+            Forgot your password?
+          </a>
+        </div>
+        <Button label="Sign In" icon="pi pi-user" className="w-full" />
+      </div>
+      <hr />
+    </div>
+  );
+};
+export default SignIn;
